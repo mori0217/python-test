74b197f9fffcf93b59b137fe7345373c7ef370d6
hisashi.mori <hmori0217@gmail.com>
2023-06-03 06:59:55 +0900

    lesson21から23まで作成

---
+++ section4/lesson23.py
@@ -0,0 +1,12 @@
+choose_from_two = ('A', 'B', 'C')
+# choose_from_two = ['A', 'B', 'C']
+
+answer = []
+
+# choose_from_two.append('A')
+# choose_from_two.append('C')
+answer.append('A')
+answer.append('C')
+
+print(choose_from_two)
+print(answer)

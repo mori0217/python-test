728e4f5abf310b937366c204f071f93a8b0997f3
hisashi.mori <hmori0217@gmail.com>
2023-06-03 08:35:39 +0900

    ソースコードをフォーマット

--- section4/lesson23.py
+++ section4/lesson23.py
@@ -1,12 +1,12 @@
-choose_from_two = ('A', 'B', 'C')
+choose_from_two = ("A", "B", "C")
 # choose_from_two = ['A', 'B', 'C']
 
 answer = []
 
 # choose_from_two.append('A')
 # choose_from_two.append('C')
-answer.append('A')
-answer.append('C')
+answer.append("A")
+answer.append("C")
 
 print(choose_from_two)
 print(answer)

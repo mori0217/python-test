2f71b7c247905e8d61b8fe7cbef87f09d4bcded0
hisashi.mori <hmori0217@gmail.com>
2023-07-06 08:43:28 +0900

    lesson204から205まで完了

---
+++ section15/base_manager_server.py
@@ -0,0 +1,18 @@
+import queue
+from multiprocessing.managers import BaseManager
+
+queue = queue.Queue()
+
+
+class QueueManager(BaseManager):
+    pass
+
+
+QueueManager.register("get_queue", callable=lambda: queue)
+
+manager = QueueManager(
+    address=("127.0.0.1", 50000),
+    authkey=b"abcde"
+)
+server = manager.get_server()
+server.serve_forever()
